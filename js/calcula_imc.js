73b64986c1eb27133b3f9fe7a183554e835a73ca
Anderson <anderson.msfisico@gmail.com>
2020-03-03 16:36:00 -0300

    Projeto de aprendizado javascript

---
+++ js/calcula_imc.js
@@ -0,0 +1,77 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+//var subtitulo = document.querySelector("h2")
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i=0; i < pacientes.length; i++){
+    
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
+
+    if(!pesoEhValido){
+        console.log("peso invalido!");
+        pesoEhValido = false;
+        tdImc.textContent = "peso invalido"
+        //paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido")
+    }
+    if(!alturaEhValida){
+        console.log("altura invalida! ");
+        alturaEhValida = false;
+        tdImc.textContent = "altura invalida";
+        //paciente.style.backgroundColor = "lightcoral";
+        paciente.classList.add("paciente-invalido")
+    }
+    if(pesoEhValido && alturaEhValida){
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso < 1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && altura <= 3.0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function calculaImc(peso, altura){
+
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
+
+
+//titulo.addEventListener("click", function(){
+//    console.log("Clicado pela função anomina")
+//});
+//
+//subtitulo.addEventListener("click", mostraMensagem);
+//
+//function mostraMensagem(){
+//    console.log("Ola eu fui clicado!!!");
+//}
+
